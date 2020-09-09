de8b8b15c1461d7bb094b7641ee9be31e6135df2
Peter Dennis <37886582+padevel@users.noreply.github.com>
2020-09-10 00:42:47 +1000

    Add a designated folder for logging (contents untracked) and use for clone tourny outputs

--- tests/clone_tournament.sh
+++ tests/clone_tournament.sh
@@ -5,9 +5,9 @@
 ssh $1@$2 $3 -p $4 &
 read -t 2 -p "Launching server on port $4.\n"
 # Start the bots
-socat TCP:${2}:$4 EXEC:"python ../pyclient/launch.py" &
-socat TCP:${2}:$4 EXEC:"python ../pyclient/launch.py -n the_clone" &
-socat TCP:${2}:$4 EXEC:"python ../pyclient/launch.py -n dbl_or_nuttn" &
+socat TCP:${2}:$4 EXEC:"python ../pyclient/launch.py -e" 2>../logs/penbot.log &
+socat TCP:${2}:$4 EXEC:"python ../pyclient/launch.py -n the_clone" 2>../logs/the_clone.log &
+socat TCP:${2}:$4 EXEC:"python ../pyclient/launch.py -n dbl_or_nuttn" 2>../logs/dbl_or_nuttn.log &
 # Start the control with manual input
 socat - TCP:${2}:$4
 
